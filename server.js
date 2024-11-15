de813f4be6f7bd01264220814a049329fa6936cf
adithyasn11 <adithyasn2487@gmail.com>
2024-11-15 14:26:43 +0530

    backend

---
+++ server.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://localhost:27017/';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
